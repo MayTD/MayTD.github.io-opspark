f4ce3488082523d2521f199dc2a43f77a829d9d7
May Do <may0000ff@gmail.com>
2019-05-22 18:39:56 +0000

    changes to datatypes.js

---
+++ studies/datatypes.js
@@ -0,0 +1,26 @@
+/*
+    DATATYPES:
+    
+    This is the type of value
+    
+    Javacript Types are Dynamic:
+    var x;          -> undefined
+    x = 5;          -> x is now a number
+    x = "Hello";    -> x is now a string
+    
+    Simple/Primitive:
+    immutable and copy by value
+    number      -> 1, 12, 5.7
+    string      -> "Hello World!"
+    boolean     -> true or false
+    NaN         -> not a number
+    undefined   -> an undefined number
+
+*/
+
+// Numbers //
+    var num1 = 3;           //whole number
+    var num2 = 3.15;        //with decimals
+    var num3 = 123e5;       // 12300000
+    var num4 = 123e-5;      // 0.00123
+    
